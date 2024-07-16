2ad9694a983f3f76878119c88a928404c49ba5c8
shirin ebrahimi <shirinebrahimi@deloitte.com>
2024-07-16 16:45:00 +0100

    creates users and schedules tables

---
+++ coordination-app/src/main/java/office/coordination/app/CoordinationAppApplication.java
@@ -0,0 +1,13 @@
+package office.coordination.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordinationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoordinationAppApplication.class, args);
+	}
+
+}
