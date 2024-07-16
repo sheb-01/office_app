ecbe78c6cb2133434e734dddd361ed55bd41faec
YuanmengLiu <yuanmengliu@uk-mg9qxw660h.uk.deloitte.com>
2024-07-16 16:39:50 +0100

    add react project

---
+++ react_app/public/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import UserList from './components/UserList';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <UserList />
+ 
+    </div>
+  );
+}
+
+export default App;
